9c6db768b10a9e90005e426a7f4034c5369b99ec
Kostantinos Kanaris <galileo@Kostantinoss-MacBook-Pro.local>
2024-03-08 01:34:53 +0200

    refactor: Change project's title and summary

--- text_multiclass_classification/__about__.py
+++ text_multiclass_classification/__about__.py
@@ -9,9 +9,9 @@ __MAJOR__ = 0
 __MINOR__ = 1
 __PATCH__ = 0
 
-__title__ = os.path.basename(os.getcwd()).replace("-", "_")
+__title__ = "text_multiclass_classification"
 __version__ = ".".join([str(__MAJOR__), str(__MINOR__), str(__PATCH__)])
-__summary__ = "A pytorch template for machine learning projects."
+__summary__ = "A multi-class text classification framework with PyTorch."
 __author__ = "Konstantinos Kanaris"
 __copyright__ = f"Copyright (C) {datetime.now().date().year}  {__author__}"
 __email__ = "konskan95@outlook.com.gr"
